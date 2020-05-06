f778a566864206cb2998666ad810ea784fb8900a
juanayestas <juanca.ayestas.0500@gmail.com>
2020-05-05 23:20:14 -0600

    deploy

--- palawiki/__init__.py
+++ palawiki/__init__.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -14,15 +15,16 @@ login_manager.login_message_category = 'info'
 
 mail = Mail()
 
+
 def create_app(Config_class=Config):
   app = Flask(__name__)
   app.config.from_object(Config)
-  
+
   db.init_app(app)
   bcrypt.init_app(app)
   login_manager.init_app(app)
   mail.init_app(app)
-  
+
   from palawiki.users.routes import users
   from palawiki.champions.routes import champions
   from palawiki.main.routes import main
@@ -31,5 +33,5 @@ def create_app(Config_class=Config):
   app.register_blueprint(champions)
   app.register_blueprint(main)
   app.register_blueprint(errors)
-  
+
   return app
